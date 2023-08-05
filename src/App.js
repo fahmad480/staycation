3214bc84046a372eb1ed9145a6a842b15423bd37
Faraaz A Permadi <33687170+fahmad480@users.noreply.github.com>
2023-08-05 16:45:08 +0700

    Create Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "assets/scss/style.scss";
+import Landingpage from "pages/LandingPage";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={Landingpage} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
+import Header from "parts/Header";
+
 export default class Landingpage extends Component {
   render() {
-    return <div>Landingpage</div>;
+    return (
+      <>
+        <Header {...this.props}></Header>
+      </>
+    );
   }
 }
