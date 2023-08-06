b42f102e4eee958da443ef23f8921a1b6ae4da23
Faraaz A Permadi <33687170+fahmad480@users.noreply.github.com>
2023-08-06 22:42:03 +0700

    create input date (night)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "assets/scss/style.scss";
 import Landingpage from "pages/LandingPage";
+import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={Landingpage} />
+        <Route path="/" exact component={Landingpage} />
+        <Route path="/example" component={Example} />
       </Router>
     </div>
   );
